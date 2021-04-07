74ffbb064052eeb110ec8801f257c9a9896eaf97
Polina <sirotkina.polina.2001@gmail.com>
2021-04-07 09:15:11 +0300

    fix input

--- lab_06/src/read.asm
+++ lab_06/src/read.asm
@@ -5,7 +5,8 @@ PUBLIC NUMBER
 
 DSEG SEGMENT PARA PUBLIC 'DATA'
 	NUMBER DW 0
-	INPUT_MSG DB "Enter signed number in binary notation (from 0 to 65535): $"
+	INPUT_MSG DB 13, 10, 13, 10, "Enter signed 16-digit number in binary notation: $"
+	RES_MSG DB 13, 10, "Number entered. $"
 DSEG ENDS
 
 CSEG SEGMENT PARA PUBLIC 'CODE'
@@ -15,6 +16,7 @@ READ_ACTION PROC NEAR
 	MOV AH, 01h
 	INT 21h
 	SUB AL, '0'
+	SUB AL, 1 ;Корректировка индексации
 	MOV CL, 2 
 	MUL CL ;Умножаем индекс на 2, т.к. каждая команда занимает в памяти 2 байта
 	MOV SI, AX
@@ -22,7 +24,7 @@ READ_ACTION PROC NEAR
 READ_ACTION ENDP
 
 READ_SB_NUM PROC NEAR
-	
+
 	MOV AH, 09H 
 	MOV DX, OFFSET INPUT_MSG
 	INT 21H
@@ -40,10 +42,11 @@ READ_SB_NUM PROC NEAR
 		MOV DI, 1 ;Признак того, что введено отрицательное число
 		JMP READ_DIGIT
 		
+		PROC_DIGIT:
+		
 		CMP AL, 13 ;Признак того, что нажата клавиша ENTER
 		JE END_READ
 		
-		PROC_DIGIT:
 		SUB AL, '0'
 		SAL DX, 1
 		ADD DL, AL
@@ -52,6 +55,10 @@ READ_SB_NUM PROC NEAR
 		JNZ READ_DIGIT
 	
 	END_READ:
+		XOR AX, AX
+		MOV AH, 09h
+		MOV DX, OFFSET RES_MSG
+		INT 21h
 		RET
 		
 READ_SB_NUM ENDP
